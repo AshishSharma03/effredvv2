e69550e0ee2ac5da8681a99eed71e67fc5c4e172
Ashish_sharma <46992681+AshishSharma03@users.noreply.github.com>
2026-01-01 08:42:12 +0000

    add_sixth_landing_page

--- components/pages/home/Landing_fifth_section.js
+++ components/pages/home/Landing_fifth_section.js
@@ -10,7 +10,6 @@ export default function DevelopmentProcessSection() {
         py: 0,
       }}
     >
-
       <Box sx={{ height: "60vh" }} />
 
       <Box
@@ -30,14 +29,14 @@ export default function DevelopmentProcessSection() {
           }}
         >
           <Typography
-          color="#000"
-           variant="body1"
-           sx={{ fontSize: "30px", fontWeight: 800, lineHeight: "1.8px" }}
+            color="#000"
+            variant="body1"
+            sx={{ fontSize: "30px", fontWeight: 800, lineHeight: "1.8px" }}
           >
             Our Development Process
           </Typography>
           <Typography
-          color="#000"
+            color="#000"
             variant="body1"
             sx={{ fontSize: "50px", fontWeight: 800, mt: 1 }}
           >
@@ -46,64 +45,331 @@ export default function DevelopmentProcessSection() {
         </Box>
 
         {/* Cards/boxes (they scroll as you scroll the page) */}
-        <Box sx={{ mt: 100 , position:"relative", display:"flex", flexDirection:"column", alignItems:"center",gap:"100px"}} >
+        <Box
+          sx={{
+            mt: 100,
+            position: "relative",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "100px",
+          }}
+        >
+          <Box
+            sx={{
+              height: "2400px",
+              position: "relative",
+              width: "100%",
+              zIndex: 99,
+            }}
+          >
             <Box
               sx={{
-                  height: "2400px",
-                  position: "relative",
-                  width: "100%",
-                   zIndex: 99,
+                background: "#ffffffff",
+                boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
+                position: "absolute",
+                width: "530px",
+                height: "400px",
+                left: "150px",
+                top: "0px",
+
+                p: 2,
               }}
             >
-              <Box sx={{background: "#ffffffff",
-           boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
-                position: "absolute",
-                width: "530px",
-                height: "430px",
-                left:"150px",
-                top:"0px",
-                
-                p: 2,}}>
-                             <Typography color="black" variant="body1" sx={{letterSpacing:"2px",lineHeight:"40px",fontSize:"25px",fontWeight:"700",p:"0px 0px 0px 10px"}}>TOP</Typography> 
-                  </Box>
+               
+                <Box
+                  sx={{
+                    position: "absolute",
+                    right: "90px",
+                    top: "90px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    color="black"
+                    variant="body1"
+                    sx={{
+                      letterSpacing: "2px",
+                      lineHeight: "40px",
+                      fontSize: "40px",
+                      fontWeight: "800",
+                      // p: "0px 0px 0px 10px",
+                      zIndex: 2,
+                      color: "#fff",
+                    }}
+                  >
+                    1
+                  </Typography>
+                  <Box sx={{ position: "absolute", width: "80px", height: "80px", background: "linear-gradient(93deg,rgba(141, 141, 141, 1) 0%, rgba(0, 0, 0, 1) 100%);",borderRadius:"100%" }} />
+                  <Box sx={{ position: "absolute", width: "100px", height: "100px",borderRadius:"100%",border:"1px solid #000" }} />
+                  <Box sx={{ position: "absolute", width: "120px", height: "120px",borderRadius:"100%",border:"1px solid #000" }} />
                   
-                <Box sx={{background: "#ffffffff",
-            boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
-                position: "absolute",
-                width: "530px",
-                height: "430px",
-                right:"150px",
-                top:"350px",
-              
-                p: 2,}}/>
+                </Box>
+                <Stack sx={{pt:"50px",px:"20px"}}>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    letterSpacing: "2px",
+                    lineHeight: "40px",
+                    fontSize: "40px",
+                    fontWeight: "800",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                  Discovery
+                  <br />& Planning
+                </Typography>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    mt:"80px",
+                    letterSpacing: "2px",
+                    lineHeight: "30px",
+                    fontSize: "20px",
+                    fontWeight: "200",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                 We explore your vision, goals, and market to create a strategic roadmap through meetings, assessments, and planning.
+                </Typography>
+                </Stack>
+            </Box>
 
-                  <Box sx={{background: "#ffffffff",
+            <Box
+              sx={{
+                background: "#ffffffff",
                 boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
                 position: "absolute",
                 width: "530px",
-                height: "430px",
-                left:"150px",
-                top:"820px",
-              
-                p: 2,}}/>
+                height: "400px",
+                right: "150px",
+                top: "350px",
 
-                  <Box sx={{background: "#ffffffff",
-                boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
-                position: "absolute",
-                width: "530px",
-                height: "430px",
-                right:"300px",
-                top:"1300px",
-             
-                p: 2,}}/>
+                p: 2,
+              }}
+            >
+                              <Box
+                  sx={{
+                    position: "absolute",
+                    right: "0px",
+                    top: "0px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    color="black"
+                    variant="body1"
+                    sx={{
+                      letterSpacing: "2px",
+                      lineHeight: "40px",
+                      fontSize: "40px",
+                      fontWeight: "800",
+                      // p: "0px 0px 0px 10px",
+                      zIndex: 2,
+                      color: "#fff",
+                    }}
+                  >
+                    2
+                  </Typography>
+                  <Box sx={{ position: "absolute", width: "80px", height: "80px", background: "linear-gradient(93deg,rgba(141, 141, 141, 1) 0%, rgba(0, 0, 0, 1) 100%);",borderRadius:"100%" }} />
+                  <Box sx={{ position: "absolute", width: "100px", height: "100px",borderRadius:"100%",border:"1px solid #000" }} />
+                  <Box sx={{ position: "absolute", width: "120px", height: "120px",borderRadius:"100%",border:"1px solid #000" }} />
                   
-              
-
+                </Box>
+                <Stack sx={{pt:"50px",px:"20px"}}>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    letterSpacing: "2px",
+                    lineHeight: "40px",
+                    fontSize: "40px",
+                    fontWeight: "800",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                  Design
+                  <br />& Prototyping
+                </Typography>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    mt:"80px",
+                    letterSpacing: "2px",
+                    lineHeight: "30px",
+                    fontSize: "20px",
+                    fontWeight: "200",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                 Wireframes and mockups bring your product to life with intuitive, user-focused design ready for seamless implementation.
+                </Typography>
+                </Stack>
 
             </Box>
-              
-     
+
+            <Box
+              sx={{
+                background: "#ffffffff",
+                boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
+                position: "absolute",
+                width: "530px",
+                height: "400px",
+                left: "150px",
+                top: "820px",
+
+                p: 2,
+              }}
+            >
+
+                 <Box
+                  sx={{
+                    position: "absolute",
+                    left: "0px",
+                    top: "0px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    color="black"
+                    variant="body1"
+                    sx={{
+                      letterSpacing: "2px",
+                      lineHeight: "40px",
+                      fontSize: "40px",
+                      fontWeight: "800",
+                      // p: "0px 0px 0px 10px",
+                      zIndex: 2,
+                      color: "#fff",
+                    }}
+                  >
+                    3
+                  </Typography>
+                  <Box sx={{ position: "absolute", width: "80px", height: "80px", background: "linear-gradient(93deg,rgba(141, 141, 141, 1) 0%, rgba(0, 0, 0, 1) 100%);",borderRadius:"100%" }} />
+                  <Box sx={{ position: "absolute", width: "100px", height: "100px",borderRadius:"100%",border:"1px solid #000" }} />
+                  <Box sx={{ position: "absolute", width: "120px", height: "120px",borderRadius:"100%",border:"1px solid #000" }} />
+                  
+                </Box>
+                <Stack sx={{pt:"50px",px:"20px"}}>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    letterSpacing: "2px",
+                    lineHeight: "40px",
+                    fontSize: "40px",
+                    fontWeight: "800",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                  QA
+                  <br />& Development
+                </Typography>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    mt:"80px",
+                    letterSpacing: "2px",
+                    lineHeight: "30px",
+                    fontSize: "20px",
+                    fontWeight: "200",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                We build clean, scalable code supported by testing plans and bug fixing to ensure stability, performance, and reliability.                  </Typography>
+                </Stack>
+            </Box>
+
+            <Box
+              sx={{
+                background: "#ffffffff",
+                boxShadow: "0px 0px 121px 53px rgba(0, 0, 0, 0.1)",
+                position: "absolute",
+                width: "530px",
+                height: "400px",
+                right: "300px",
+                top: "1300px",
+
+                p: 2,
+              }}
+            >
+                              <Box
+                  sx={{
+                    position: "absolute",
+                    left: "45%",
+                    top: "-10px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    color="black"
+                    variant="body1"
+                    sx={{
+                      letterSpacing: "2px",
+                      lineHeight: "40px",
+                      fontSize: "40px",
+                      fontWeight: "800",
+                      // p: "0px 0px 0px 10px",
+                      zIndex: 2,
+                      color: "#fff",
+                    }}
+                  >
+                    4
+                  </Typography>
+                  <Box sx={{ position: "absolute", width: "80px", height: "80px", background: "linear-gradient(93deg,rgba(141, 141, 141, 1) 0%, rgba(0, 0, 0, 1) 100%);",borderRadius:"100%" }} />
+                  <Box sx={{ position: "absolute", width: "100px", height: "100px",borderRadius:"100%",border:"1px solid #000" }} />
+                  <Box sx={{ position: "absolute", width: "120px", height: "120px",borderRadius:"100%",border:"1px solid #000" }} />
+                  
+                </Box>
+                <Stack sx={{pt:"50px",px:"20px"}}>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    letterSpacing: "2px",
+                    lineHeight: "40px",
+                    fontSize: "40px",
+                    fontWeight: "800",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                  Agile
+                  <br />Development
+                </Typography>
+                 <Typography
+                  color="black"
+                  variant="body1"
+                  sx={{
+                    mt:"80px",
+                    letterSpacing: "2px",
+                    lineHeight: "30px",
+                    fontSize: "20px",
+                    fontWeight: "200",
+                    p: "0px 0px 0px 10px",
+                  }}
+                >
+                Agile sprints drive continuous improvement. We support product launch, PR, investor readiness, and long-term growth strategies.
+                </Typography>
+                </Stack>
+            </Box>
+          </Box>
         </Box>
+
+
+
+        
       </Box>
 
       {/* More content after section */}
--- components/pages/home/Landing_sixth_section.js
+++ components/pages/home/Landing_sixth_section.js
@@ -1,10 +1,39 @@
-import { Box } from '@mui/material'
-import React from 'react'
+"use client";
+
+import { Box ,Typography} from "@mui/material";
+import React, { useEffect, useRef } from "react";
+
 
 function Landing_sixth_section() {
+
+
+
   return (
-    <Box sx={{minHeight: "100vh",background:"#000"  }}>Landing_sixth_section</Box>
-  )
+    <Box sx={{ position: "relative", minHeight: "100vh", background: "#000", overflow: "hidden" }}>
+      <Box
+        component="video"
+        src="./video/wave.mkv"
+        autoPlay
+        loop
+        muted
+        playsInline
+        sx={{
+          position: "absolute",
+          inset: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          opacity: 0.7,
+        }}
+      />
+
+      {/* Content above video */}
+      <Box sx={{ position: "relative", zIndex: 1, display: "flex",flexDirection:"column", justifyContent: "center",textAlign:"center",alignItems: "center",minHeight:"100vh" }}>
+         <Typography color="white" variant="body1 " sx={{letterSpacing:"2px",fontSize:"24px",fontWeight:"200",mb:"20px"}}>[ AI Automation ]</Typography>
+         <Typography color="white" variant="body1 " sx={{letterSpacing:"2px",lineHeight:"50px",fontSize:"50px",fontWeight:"200"}}>AI automation that moves your<br/> business forward</Typography>
+      </Box>
+    </Box>
+  );
 }
 
-export default Landing_sixth_section
+export default Landing_sixth_section;
